45c2fe4b1156066ef9649b4e0c37c9ae561fd353
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-09-04 17:32:29 -0400

    added nav bar, cover image

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+// Import picture froma assets
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+      <div className="my-2">
+        <p>Insert text here.</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
